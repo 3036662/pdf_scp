4da9b48c00b29d652b088c283ca9e0ee3809e741
Oleg Proskurin <proskur@altlinux.org>
2024-07-13 23:52:20 +0300

    [draft] FindSignature method

--- pdf/pdf.cpp
+++ pdf/pdf.cpp
@@ -1,10 +1,20 @@
 #include "pdf.hpp"
 #include <filesystem>
+#include <memory>
+#include <qpdf/QPDF.hh>
+#include <qpdf/QPDFObjectHandle.hh>
 #include <stdexcept>
+#include <string>
 
 #include "common_defs.hpp"
 
 namespace pdfcsp::pdf {
+
+Pdf::Pdf() : qpdf_(std::make_unique<QPDF>()) {}
+
+Pdf::Pdf(const std::string &path) : qpdf_(std::make_unique<QPDF>()) {
+  Open(path);
+}
 
 void Pdf::Open(const std::string &path) {
   namespace fs = std::filesystem;
@@ -17,10 +27,53 @@ void Pdf::Open(const std::string &path) {
   if (fs::file_size(path) > kMaxPdfFileSize) {
     throw std::logic_error("file is too big");
   }
+  qpdf_->processFile(path.c_str());
+}
 
-  // not existing
-  // cant open for reading
-  //  not enough memory to read
+bool Pdf::FindSignature() noexcept {
+  ObjHandler obj_root = std::make_unique<QPDFObjectHandle>(qpdf_->getRoot());
+  if (obj_root->isNull()) {
+    return false;
+  } else {
+    root_ = std::move(obj_root);
+  }
+  std::string tag_acro(kTagAcroForm);
+  if (!root_->hasKey(tag_acro)) {
+    return false;
+  } else {
+    acroform_ = std::make_unique<QPDFObjectHandle>(root_->getKey(tag_acro));
+    if (acroform_->isNull()) {
+      return false;
+    }
+  }
+  if (!acroform_->isDictionary()) {
+    Log("No DICT in AcroForm\n");
+    return false;
+  }
+  std::string tag_fields(kTagFields);
+  if (!acroform_->hasKey(tag_fields)) {
+    Log("No fields in the AcroForm\n");
+    return false;
+  }
+  auto acro_fields = acroform_->getKey(tag_fields);
+  if (!acro_fields.isArray()) {
+    Log("Acro /Fields is not an array\n");
+    return false;
+  }
+  return true;
+}
+
+// ---------------------------------------------------
+// private
+
+void Pdf::Log(const char *msg) const noexcept {
+  if (std_err_flag_) {
+    std::cerr << "[Pdf]" << msg << "\n";
+  }
+}
+
+inline void Pdf::Log(const std::string &msg) const noexcept {
+  Log(msg.c_str());
 }
 
 } // namespace pdfcsp::pdf
--- pdf/pdf.hpp
+++ pdf/pdf.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <memory>
+#define POINTERHOLDER_TRANSITION 3
+#include <qpdf/QPDF.hh>
+#include <qpdf/QPDFAcroFormDocumentHelper.hh>
+#include <qpdf/QPDFAnnotationObjectHelper.hh>
+#include <qpdf/QPDFObjectHandle.hh>
+#include <qpdf/QPDFPageObjectHelper.hh>
+#include <qpdf/QUtil.hh>
 #include <string>
 #include <vector>
 
@@ -7,12 +15,41 @@ namespace pdfcsp::pdf {
 
 class Pdf {
 public:
+  using ObjHandler = std::unique_ptr<QPDFObjectHandle>;
+
+  /**
+   * @brief Construct a new Pdf object
+   * @throws propagateed exceptions
+   */
+  Pdf();
+
+  /**
+   * @brief Construct a new Pdf object
+   *
+   * @param path to file
+   * @throws if cant open file
+   */
+  Pdf(const std::string &path);
+
+  Pdf(const Pdf &) = delete;
+  Pdf(Pdf &&) = delete;
+  Pdf &operator=(const Pdf &) = delete;
+  Pdf &operator=(Pdf &&) = delete;
+
   /**
    * @brief Open a pdf file
    * @param path string path to file
    * @throws std::logical_error if file doesn't exist or can't open
    */
   void Open(const std::string &path);
+
+  /**
+   * @brief true if some Signatures found
+   *
+   * @return true
+   * @return false
+   */
+  [[nodiscard]] bool FindSignature() noexcept;
 
   /**
    * @brief Get the Raw Signature data
@@ -26,7 +63,26 @@ public:
    */
   std::vector<unsigned char> getRawData() noexcept;
 
+  /**
+   * @brief Turn off/on logging to a stderr
+   * @param val true/false
+   */
+  void EnableLogToStdErr(bool val) noexcept { std_err_flag_ = val; }
+
 private:
+  static constexpr const char *const kTagAcroForm = "/Acroform";
+  static constexpr const char *const kTagFields = "/Fields";
+
+  void Log(const char *msg) const noexcept;
+  inline void Log(const std::string &msg) const noexcept;
+
+  std::unique_ptr<QPDF> qpdf_;
+
+  // default on Construct
+  std::unique_ptr<QPDFObjectHandle> root_;
+  std::unique_ptr<QPDFObjectHandle> acroform_;
+  std::unique_ptr<QPDFObjectHandle> signature_value_;
+  bool std_err_flag_ = true;
 };
 
 } // namespace  pdfcsp::pdf
