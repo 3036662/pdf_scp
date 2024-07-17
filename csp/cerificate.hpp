2dfbd23bdf0e592a4fe228fe1b26c36745a8845f
Oleg Proskurin <proskur@altlinux.org>
2024-07-17 12:17:11 +0300

    add Certificate structure

---
+++ csp/cerificate.hpp
@@ -0,0 +1,14 @@
+#pragma  once
+
+#include <string>
+#include <vector>
+
+namespace pdfcsp::csp {
+
+
+struct CertificateID{
+    std::vector<unsigned char> serial;
+    std::string issuer;
+};
+
+} //namespace pdfcsp::csp 
