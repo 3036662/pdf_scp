81f147ddae843d68779ac71a314f94d752ea85ff
Oleg Proskurin <proskur@altlinux.org>
2024-08-08 10:15:08 +0300

    move base check to Test function

--- csp/test/test_valid_bunch.cpp
+++ csp/test/test_valid_bunch.cpp
@@ -20,8 +20,7 @@ using namespace pdfcsp::csp::asn;
 constexpr const char *const test_file_dir = TEST_FILES_DIR;
 const std::string test_dir = std::string(test_file_dir) + "valid_files/";
 
-TEST_CASE("UnparseARM") {
-  const std::string file = test_dir + "02_cam_BES.pdf";
+void Test(const std::string &file, CadesType cad_type) {
   std::cout << "File: " << file << "\n";
   pdfcsp::pdf::Pdf pdf;
   pdfcsp::csp::Csp csp;
@@ -29,53 +28,49 @@ TEST_CASE("UnparseARM") {
   REQUIRE_NOTHROW(pdf.Open(file));
   REQUIRE_NOTHROW(pdf.FindSignatures());
   for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-    AsnObj obj(pdf.getRawSignature(i).data(), pdf.getRawSignature(i).size(),
-               std::make_shared<ResolvedSymbols>());
-    auto unparsed = obj.Unparse();
-    std::cout << "Initial size " << pdf.getRawSignature(i).size() << "\n";
-    std::cout << "Unparsed size " << unparsed.size() << "\n";
-    if (unparsed.size() < pdf.getRawSignature(i).size()) {
-      unparsed.resize(pdf.getRawSignature(i).size(), 0x00);
+    std::cout << "\nTest signature " << i + 1 << " of "
+              << pdf.GetSignaturesCount() << "\n";
+    REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
+    std::ofstream outp_file(
+        "08_cam_CADEST_signers_free_area_plus_sign_not_in_signer.dat" +
+            std::to_string(i),
+        std::ios_base::binary);
+
+    for (const auto ch : pdf.getRawSignature(i)) {
+      outp_file << ch;
     }
-    REQUIRE(unparsed.size() == pdf.getRawSignature(i).size());
-    REQUIRE(unparsed == pdf.getRawSignature(i));
+    outp_file.close();
+    REQUIRE(msg);
+    std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
+              << "\n";
+    auto signers = msg->GetSignersCount();
+    REQUIRE(signers);
+    REQUIRE(*signers > 0);
+    std::cout << "Signers number " << signers.value_or(0) << "\n";
+    auto revoces_count = msg->GetRevokedCertsCount();
+    REQUIRE(revoces_count);
+    std::cout << "Revoces number " << revoces_count.value() << "\n";
+    for (uint signer_index = 0; signer_index < signers.value();
+         ++signer_index) {
+      REQUIRE(msg->GetCadesTypeEx(signer_index) == cad_type);
+      std::cout << "Type:"
+                << InternalCadesTypeToString(msg->GetCadesTypeEx(signer_index))
+                << "\n";
+      REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
+    }
   }
+}
+
+TEST_CASE("UnparseARM") {
+  const std::string file = test_dir + "02_cam_BES.pdf";
+  Test(file, pdfcsp::csp::CadesType::kCadesBes);
 }
 
 TEST_CASE("BES1") {
 
   SECTION("01_okular") {
     const std::string file = test_dir + "01_okular_BES.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesBes);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->CheckDataHash(pdf.getRawData(i), signer_index));
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesBes);
   }
 }
 
@@ -124,44 +119,7 @@ TEST_CASE("BES2") {
 TEST_CASE("BES3") {
   SECTION("03_cam_BES_signers_free_area") {
     const std::string file = test_dir + "03_cam_BES_signers_free_area.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file("03_cam_BES_signers_free_area.dat" +
-                                  std::to_string(i),
-                              std::ios_base::binary);
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesBes);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesBes);
   }
   SECTION("Bad data") {
     const std::string file = test_dir + "03_cam_BES_signers_free_area.pdf";
@@ -186,89 +144,14 @@ TEST_CASE("BES4") {
   SECTION("04_cam_BES_signers_free_area_signed_BES") {
     const std::string file =
         test_dir + "04_cam_BES_signers_free_area_signed_BES.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file("04_cam_BES_signers_free_area_signed_BES.dat" +
-                                  std::to_string(i),
-                              std::ios_base::binary);
-
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesBes);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesBes);
   }
 }
 
 TEST_CASE("BES5") {
   SECTION("05_acrob_BES") {
     const std::string file = test_dir + "05_acrob_BES.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file("05_acrob_BES.dat" + std::to_string(i),
-                              std::ios_base::binary);
-
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesBes);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesBes);
   }
   SECTION("Bad data") {
     const std::string file = test_dir + "05_acrob_BES.pdf";
@@ -309,87 +192,14 @@ TEST_CASE("BES5") {
 TEST_CASE("T06") {
   SECTION("06_cam_CADEST_signers_free_area") {
     const std::string file = test_dir + "06_cam_CADEST_signers_free_area.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file("06_cam_CADEST_signers_free_area.dat" +
-                                  std::to_string(i),
-                              std::ios_base::binary);
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesT);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesT);
   }
 }
 
 TEST_CASE("T07") {
   SECTION("07_acrob_CADEST") {
     const std::string file = test_dir + "07_acrob_CADEST.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file("07_acrob_CADEST.dat" + std::to_string(i),
-                              std::ios_base::binary);
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesT);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesT);
   }
 }
 
@@ -398,45 +208,6 @@ TEST_CASE("T08") {
     const std::string file =
         test_dir +
         "08_cam_CADEST_signers_free_area_plus_sign_not_in_signer.pdf";
-    std::cout << "File: " << file << "\n";
-    pdfcsp::pdf::Pdf pdf;
-    pdfcsp::csp::Csp csp;
-    PtrMsg msg;
-    REQUIRE_NOTHROW(pdf.Open(file));
-    REQUIRE_NOTHROW(pdf.FindSignatures());
-    for (uint i = 0; i < pdf.GetSignaturesCount(); ++i) {
-      std::cout << "\nTest signature " << i + 1 << " of "
-                << pdf.GetSignaturesCount() << "\n";
-      REQUIRE_NOTHROW(msg = csp.OpenDetached(pdf.getRawSignature(i)));
-      std::ofstream outp_file(
-          "08_cam_CADEST_signers_free_area_plus_sign_not_in_signer.dat" +
-              std::to_string(i),
-          std::ios_base::binary);
-
-      for (const auto ch : pdf.getRawSignature(i)) {
-        outp_file << ch;
-      }
-      outp_file.close();
-      REQUIRE(msg);
-      REQUIRE(msg->GetCadesType() == CadesType::kCadesBes);
-      std::cout << "Type:" << InternalCadesTypeToString(msg->GetCadesType())
-                << "\n";
-      auto signers = msg->GetSignersCount();
-      REQUIRE(signers);
-      REQUIRE(*signers > 0);
-      std::cout << "Signers number " << signers.value_or(0) << "\n";
-      auto revoces_count = msg->GetRevokedCertsCount();
-      REQUIRE(revoces_count);
-      std::cout << "Revoces number " << revoces_count.value() << "\n";
-      for (uint signer_index = 0; signer_index < signers.value();
-           ++signer_index) {
-        REQUIRE(msg->GetCadesTypeEx(signer_index) == CadesType::kCadesT);
-        std::cout << "Type:"
-                  << InternalCadesTypeToString(
-                         msg->GetCadesTypeEx(signer_index))
-                  << "\n";
-        REQUIRE(msg->Check(pdf.getRawData(i), signer_index, true));
-      }
-    }
+    Test(file, pdfcsp::csp::CadesType::kCadesT);
   }
 }
