161fe2e35e3875468f7d33bd5bb3dca7bf7206dc
Oleg Proskurin <proskur@altlinux.org>
2024-08-19 18:26:53 +0300

    parse CRLs in RevocationValues

--- csp/asn1/revoc_vals.cpp
+++ csp/asn1/revoc_vals.cpp
@@ -1,7 +1,9 @@
 #include "asn1.hpp"
+#include "cms.hpp"
 #include <iostream>
 #include <revoc_vals.hpp>
 #include <stdexcept>
+#include <vector>
 
 namespace pdfcsp::csp::asn {
 
@@ -11,16 +13,33 @@ RevocationValues::RevocationValues(const AsnObj &obj) {
   }
   for (const auto &child : obj.Childs()) {
     const uint choice = child.ParseChoiceNumber();
-    // TODO(Oleg) implement choice 0 and 2
-    if (choice != 1) {
+    switch (choice) {
+    case 0: // crlVals
+    {
+      std::vector<CertificateList> res;
+      const AsnObj cert_lists_asn = child.ParseAs(AsnTag::kSequence);
+      if (cert_lists_asn.Childs().empty()) {
+        return;
+      }
+      for (const auto &list_asn : cert_lists_asn.at(0).Childs()) {
+        res.emplace_back(list_asn);
+      }
+      crlVals = std::move(res);
+      // cert_list_asn.PrintInfo();
+      break;
+    }
+    case 1: { // ocspVals
+      std::vector<BasicOCSPResponse> res;
+      const AsnObj ocsp_vals_asn = child.ParseAs(AsnTag::kSequence).at(0);
+      for (const auto &ocsp_val : ocsp_vals_asn.Childs()) {
+        res.emplace_back(ocsp_val);
+      }
+      ocspVals = std::move(res);
+      break;
+    }
+    default:
       throw std::runtime_error("[RevocationValues] unsupported choice");
     }
-    std::vector<BasicOCSPResponse> res;
-    const AsnObj ocsp_vals_asn = obj.at(0).ParseAs(AsnTag::kSequence).at(0);
-    for (const auto &ocsp_val : ocsp_vals_asn.Childs()) {
-      res.emplace_back(ocsp_val);
-    }
-    ocspVals = std::move(res);
   }
 }
 
