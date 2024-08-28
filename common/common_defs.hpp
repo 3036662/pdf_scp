8ff5040c583b97dab0dd63718d6f1ae91dac0c5b
Oleg Proskurin <proskur@altlinux.org>
2024-08-28 12:13:07 +0300

    Add macros LIB_API and LIB_LOCAL for library API visibility.

--- common/common_defs.hpp
+++ common/common_defs.hpp
@@ -1,3 +1,7 @@
 
 #include <cstdint>
 constexpr uint64_t kMaxPdfFileSize=2147483648; //  2GB
+
+
+#define LIB_API __attribute__((visibility("default")))
+#define LIB_LOCAL __attribute__((visibility("hidden")))
