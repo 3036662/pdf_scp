40a6f782183791e301a3f30fda9e45621ad8fef3
Oleg Proskurin <proskur@altlinux.org>
2024-11-05 18:00:26 +0300

    string error codes for qml

--- common/common_defs.hpp
+++ common/common_defs.hpp
@@ -6,5 +6,6 @@ constexpr uint64_t kMaxPdfFileSize=2147483648; //  2GB
 #define LIB_API __attribute__((visibility("default")))
 #define LIB_LOCAL __attribute__((visibility("hidden")))
 
-const char* const kErrExpiredCert="Expired certificate";
-const char* const kErrMayBeTspInvalid="Error,may be the TSP url is invalid";
+// string error codes for qml frontend
+const char* const kErrExpiredCert="CERT_EXPIRED";
+const char* const kErrMayBeTspInvalid="MAYBE_TSP_URL_INVALID";
