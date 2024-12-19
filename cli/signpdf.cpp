aca0f217d7c31e00a850ad8cbf660310dd411955
Oleg Proskurin <proskur@altlinux.org>
2024-12-19 08:13:09 +0300

    setup logger

--- cli/signpdf.cpp
+++ cli/signpdf.cpp
@@ -1,26 +1,29 @@
 #include "options.hpp"
-#include "tr.hpp"
 #include <iostream>
 #include <libintl.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main(int argc, char *argv[]) {
-
+  // setup the transtlator
   if (setlocale(LC_ALL, "") == nullptr) { // NOLINT
     std::cerr << "Failed to set locale.\n";
     return 1;
   }
-
   bindtextdomain(TRANSLATION_DOMAIN, TRANSLATIONS_DIR_COMPILED);
   bind_textdomain_codeset(TRANSLATION_DOMAIN, "UTF-8");
   textdomain(TRANSLATION_DOMAIN);
-
   // print help
   const pdfcsp::cli::Options options(argc, argv);
   if (options.help() || !options.AllMandatoryAreSet()) {
     return 0;
   }
+  // setup logging
+  auto console = spdlog::stdout_color_mt(TRANSLATION_DOMAIN);  
+  if (!console){
+    std::cerr <<tr("Setup logger failed");
+  }  
+  console->info("")
 
-  // check if all options are set
 
   return 0;
 }
